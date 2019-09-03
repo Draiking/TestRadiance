5aad5b4d69a434e8e14c2ab127af3027ddf9610a
Gennadya <heavyyrain3039@gmail.com>
2019-09-03 03:12:14 +0300

    validator

--- src/js/common.js
+++ src/js/common.js
@@ -4,4 +4,14 @@ $(document).ready(function () {
         $(".modal").addClass('fade');
     });
 
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        dots: true,
+        loop: true,
+    });
+
+    var email = document.getElementsByClassName('email');
+
+    console.log(email);
+
 });
